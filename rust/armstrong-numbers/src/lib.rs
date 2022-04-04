5a27954bcfa4428b13ff3ef5bf2c7d199aa02102
hector <hmgarcia06@gmail.com>
2022-04-04 00:19:36 -0400

    Done armstrong_numbers

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,5 +1,52 @@
-pub fn is_armstrong_number(num: u32) -> bool {
-    // unimplemented!("true if {} is an armstrong number", num);
+pub fn is_armstrong_number(number: u32) -> bool {
+    print_space();
     
-    // false
+    let armstrong_number = number == calculate_armstrong_number(number);
+    
+    println!("armstrong_number: {}", armstrong_number);
+    print_space();
+    
+    armstrong_number
+}
+
+pub fn calculate_armstrong_number(num: u32) -> u32 {
+    let number_string = num.to_string();
+    let numbers_list_chars =  number_string.chars();
+    println!("numbers_list_chars: {:?}", numbers_list_chars);
+    
+    let numbers_list = numbers_list_chars
+        .map(|d| d.to_digit(10).unwrap())
+        .collect::<Vec<_>>()
+        .into_iter();
+    println!("numbers_list: {:?}", numbers_list);
+    
+    let number_digits = numbers_list.len() as u32;
+    println!("number_digits: {}", number_digits);
+    
+    let mut counter = 0;
+    for number in numbers_list {
+        println!("number_in_list: {}", number);
+        let exponent = pow(number, number_digits);
+        counter += exponent
+    }
+    println!("counter: {}", counter);
+    counter
+}
+
+fn pow(base: u32, exponent: u32) -> u32 {
+    let pow = u32::pow(base, exponent);
+    pow
+}
+
+fn print_space() {
+    println!("                          ");
+    println!("                          ");
+    println!("                          ");
+    println!("                          ");
+    println!("                          ");
+    println!("                          ");
+    println!("                          ");
+    println!("                          ");
+    println!("*************************");
+    println!("*************************");
 }
--- rust/armstrong-numbers/tests/armstrong-numbers.rs
+++ rust/armstrong-numbers/tests/armstrong-numbers.rs
@@ -1,7 +1,7 @@
 use armstrong_numbers::*;
 
 #[test]
-#[ignore]
+// #[ignore]
 fn test_zero_is_an_armstrong_number() {
     assert!(is_armstrong_number(0))
 }
@@ -13,43 +13,61 @@ fn test_single_digit_numbers_are_armstrong_numbers() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
+fn test_nine_is_a_armstrong_number() {
+    assert!(is_armstrong_number(9))
+}
+
+#[test]
+// #[ignore]
+fn test_double_digit_numbers_are_armstrong_numbers() {
+    assert!(!(is_armstrong_number(10)))
+}
+
+#[test]
+// #[ignore]
 fn test_there_are_no_2_digit_armstrong_numbers() {
     assert!(!is_armstrong_number(10))
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn test_three_digit_armstrong_number() {
     assert!(is_armstrong_number(153))
 }
 
 #[test]
-#[ignore]
+// #[ignore]
+fn test_154_is_not_a_armstrong_number() {
+    assert!(!is_armstrong_number(154))
+}
+
+#[test]
+// #[ignore]
 fn test_three_digit_non_armstrong_number() {
     assert!(!is_armstrong_number(100))
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn test_four_digit_armstrong_number() {
     assert!(is_armstrong_number(9474))
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn test_four_digit_non_armstrong_number() {
     assert!(!is_armstrong_number(9475))
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn test_seven_digit_armstrong_number() {
     assert!(is_armstrong_number(9_926_315))
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn test_seven_digit_non_armstrong_number() {
     assert!(!is_armstrong_number(9_926_316))
 }
