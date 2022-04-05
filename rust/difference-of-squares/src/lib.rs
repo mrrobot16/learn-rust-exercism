bd9de0b8abe48b3c21c9b13ad5ea9a7077a7a22d
hector <hmgarcia06@gmail.com>
2022-04-05 01:47:46 -0400

    differenes_of_squares exercise done

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -9,19 +9,17 @@ pub fn square_of_sum(n: u32) -> u32 {
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    // unimplemented!("sum of squares of 1...{}", n);
-    3
+    let mut vector: Vec<u32> = Vec::new();
+    for  number in 1..(n+1) {
+        vector.push(u32::pow(number,2))
+    }
+    let sum: u32 = vector.iter().sum();
+    sum
 }
 
 pub fn difference(n: u32) -> u32 {
-    // unimplemented!(
-    //     "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-    //     n = n,
-    // );
-    3
+    let square_of_sum = square_of_sum(n);
+    let sum_of_squares = sum_of_squares(n);
+    let difference = square_of_sum - sum_of_squares;
+    difference
 }
-
-// pub fn total() -> u64 {
-    // let total_sum_of_grains_in_board = build_calculated_grains_board(64).iter().map(|x| x).sum();
-    // total_sum_of_grains_in_board
-// }
